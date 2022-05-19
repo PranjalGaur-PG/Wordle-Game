d4cde4d6074b2d27341283477709087f8c264a61
PranjalGaur-PG <pranjalgaur.20.12@gmail.com>
2022-05-20 02:45:19 +0530

    Complete add attempt functionality.

--- utilities/getArrays.js
+++ utilities/getArrays.js
@@ -1,32 +1,29 @@
 const getArrays = (word, response) => {
-
     function setCharAt(str, index, chr = "#") {
         if (index > str.length - 1) return str;
         return str.substring(0, index) + chr + str.substring(index + 1);
     }
-
+  
     let guess = ["Grey", "Grey", "Grey", "Grey", "Grey"];
-
-    for(var i=0; i<5; i++) {
-        if(word[i] === response[i]) {
+  
+    for (var i = 0; i < 5; i++) {
+        if (word[i] === response[i]) {
             guess[i] = "Green";
             word = setCharAt(word, i);
             response = setCharAt(response, i);
         }
-
     }
-
-    for(var i=0; i<5; i++) {
-        if(word[i] === '#') continue;
-        else if(word.indexOf(response[i]) !== -1) {
+    
+    for (var i = 0; i < 5; i++) {
+        if (response[i] === "#") continue;
+        else if (word.indexOf(response[i]) !== -1) {
             guess[i] = "Orange";
             // word[word.indexOf(response[i])] = '#';
             word = setCharAt(word, word.indexOf(response[i]));
         }
-
     }
 
     return guess;
-}
-
-module.exports = getArrays; 
+    };
+    
+    module.exports = getArrays;
