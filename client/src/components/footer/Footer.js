08355e807d7d7fa8845b4dd0b8f85e05890a166a
PranjalGaur-PG <pranjalgaur.20.12@gmail.com>
2022-05-22 22:19:23 +0530

    Fix Footer.

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -7,32 +7,23 @@ const Footer = () => {
       <footer>
         <div className="container-footer">
           <div className="row">
-            <div className="col-sm-6 col-md-6 item">
+            <div className="col-sm-6 col-md-2 item">
               <p className="copyright">Pranjal Gaur © 2022</p>
-              {/* <h3>Services</h3>
-              <ul>
-                <li>
-                  <a href="#">Web design</a>
-                </li>
-                <li>
-                  <a href="#">Development</a>
-                </li>
-                <li>
-                  <a href="#">Hosting</a>
-                </li>
-              </ul> */}
+              
             </div>
 
-            {/* <div className="col-md-8 item text">
-              <h3>Wordle Game</h3>
+            <div className="col-md-8 item text">
+              <a href="https://en.wikipedia.org/wiki/Wordle">
+                <h3 className="text-center">Wordle Game</h3>
+              </a>
               <p>
                 Wordle is simple: You have six chances to guess the day’s secret
                 five-letter word. Type in a word as a guess, and the game tells
                 you which letters are or aren’t in the word. The aim is to
                 figure out the secret word with the fewest guesses.
               </p>
-            </div> */}
-            <div className="col-md-6 col">
+            </div>
+            <div className="col-md-2 col">
               <ul className="col item social">
                 <li>
                   <a href="#">
