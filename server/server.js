4da60d07db0b340f756754a263a6bd92cd97deb8
PranjalGaur-PG <pranjalgaur.20.12@gmail.com>
2022-05-23 10:54:00 +0530

    Prepare for deployment.

--- server/server.js
+++ server/server.js
@@ -11,14 +11,14 @@ connectDB();
 app.use(express.json({extended : false}));
 app.use('/session', require('./routes/session'));
 
-if(process.env.NODE_ENV === 'production') {
-  // Set Static folder
+// if(process.env.NODE_ENV === 'production') {
+//   // Set Static folder
 
   app.use(express.static('./build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
   })
-}
+// }
 
 app.listen(port, () => console.log(`Running on Port : ${port}`));
 
