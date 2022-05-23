6264c1c6e5885f6f1bfc62ee4af77800a310211e
PranjalGaur-PG <pranjalgaur.20.12@gmail.com>
2022-05-23 10:16:16 +0530

    Prepare for deployment.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./utilities/db");
+const path = require("path");
 
 const port = process.env.PORT || 8000;
 
@@ -7,12 +8,17 @@ app = express();
 
 connectDB();
 
-app.get("/", (req, res) => {
-  res.send("Welcome to Wordle API");
-});
-
 app.use(express.json({extended : false}));
 app.use('/session', require('./routes/session'));
+
+if(process.env.NODE_ENV === 'production') {
+  // Set Static folder
+
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+  })
+}
 
 app.listen(port, () => console.log(`Running on Port : ${port}`));
 
